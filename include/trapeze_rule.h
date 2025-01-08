6d4a52fd206f277bc31b84965ccda06f344589b6
Answed <schradearwed@gmail.com>
2025-01-08 06:57:53 +0100

    Initial commit

---
+++ include/trapeze_rule.h
@@ -0,0 +1,20 @@
+﻿//
+// Created by Arwed on 1/7/2025.
+//
+
+#ifndef TRAPEZ_RULE_H
+#define TRAPEZ_RULE_H
+
+class trapeze_rule {
+public:
+    trapeze_rule(const double a, const double b, const int n) : a(a), b(b), n(n) {};
+    int calculate();
+private:
+    const double a;
+    const double b;
+    const int n;
+};
+
+
+
+#endif //TRAPEZ_RULE_H
---
+++ source/trapeze_rule.cpp
@@ -0,0 +1,13 @@
+﻿//
+// Created by Arwed on 1/7/2025.
+//
+
+#include "trapeze_rule.h"
+
+int trapeze_rule::calculate() {
+    double sum = 0;
+    for (int i = i; i <= n-1; i++) {
+        sum += (a + i * (b - a) / n);
+    }
+
+}
