cf4eeef2037efa09a34d70e10c2a61821d0e5971
Answed <schradearwed@gmail.com>
2025-02-11 17:51:48 +0100

    Completes Trapez Rule

--- include/trapeze_rule.h
+++ include/trapeze_rule.h
@@ -4,11 +4,13 @@
 
 #ifndef TRAPEZ_RULE_H
 #define TRAPEZ_RULE_H
+#include <math.h>
+#include "shunting_yard.h"
 
 class trapeze_rule {
 public:
     trapeze_rule(const double a, const double b, const int n) : a(a), b(b), n(n) {};
-    int calculate();
+    int calculate(shunting_yard& function);
 private:
     const double a;
     const double b;
--- source/trapeze_rule.cpp
+++ source/trapeze_rule.cpp
@@ -4,10 +4,11 @@
 
 #include "trapeze_rule.h"
 
-int trapeze_rule::calculate() {
+int trapeze_rule::calculate(shunting_yard& function) {
     double sum = 0;
     for (int i = i; i <= n-1; i++) {
-        sum += (a + i * (b - a) / n);
+        sum += 2 * function.calculate(a + i/n * (b - a));
     }
-
+    sum += function.calculate(a) + function.calculate(b);
+    return sum * (b - a) / (2 * n);
 }
