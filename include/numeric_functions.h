7e48d0c99efa9e29ae333e1e5e367d42c8da147b
Answed <schradearwed@gmail.com>
2025-02-11 18:54:04 +0100

    Added Simpson Regel

---
+++ include/numeric_functions.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by Arwed on 1/7/2025.
+//
+
+#ifndef TRAPEZ_RULE_H
+#define TRAPEZ_RULE_H
+#include <math.h>
+#include "shunting_yard.h"
+
+class numeric_functions {
+public:
+    numeric_functions(double a, double b, int n, int stepLength) : a(a), b(b), n(n), stepLength(stepLength) {}
+    double calculate_trapez_rule(shunting_yard& function);
+    double calculate_simpson_rule(shunting_yard& function);
+
+private:
+    const double a;
+    const double b;
+    const int n;
+    const int stepLength;
+};
+
+
+
+#endif //TRAPEZ_RULE_H
---
+++ source/numeric_functions.cpp
@@ -0,0 +1,32 @@
+﻿//
+// Created by Arwed on 1/7/2025.
+//
+
+#include "numeric_functions.h"
+
+double numeric_functions::calculate_trapez_rule(shunting_yard& function) {
+    double sum = 0;
+    for (int i = 1; i <= n-1; i++) {
+        sum += 2 * function.calculate(a + i/n * (b - a));
+    }
+    sum += function.calculate(a) + function.calculate(b);
+    return sum * (b - a) / (2 * n);
+}
+
+double numeric_functions::calculate_simpson_rule(shunting_yard& function) {
+    double sum = 0;
+    double xValue[n];
+    xValue[0] = a;
+    for (int i = 1; i <= n; i++) {
+        xValue[i] = xValue[i-1] + i/n * (b - a);
+    }
+
+    for (int i = 0; i <= n-1; i++) {
+        sum += 4 * function.calculate((xValue[i] + xValue[i+1]) / 2);
+    }
+    for (int i = 1; i <= n-1; i++) {
+        sum += 2 * function.calculate(xValue[i]);
+    }
+    sum += function.calculate(a) + function.calculate(b);
+    return sum * (b - a) / (6 * n);
+}
